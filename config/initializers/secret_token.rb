60c57c219798b4f32655f977669d96d26f6ff6d6
saikache <saisagarkache@gmail.com>
2016-01-19 19:21:55 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbPublicFeed::Application.config.secret_token = 'd042127239cf1b4eba0bca2f442259f29633664477d66cf8cba91e4bcd783fa4022b881bbfa995fe18c6a8618a032d6cadfe195b2e415256b78f74289980f2e9'
